3fcf1b1e545918726a5287f6b1b2b03224053a0e
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-10-01 21:11:53 -0300

    Revert "initial commit"
    This reverts commit eb93e37f0842e468e1dd7617586311b66ad57659.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-const rotas = require("./rotas");
-const cors = require("cors");
+const knex = require("./conexao");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(rotas);
+
+app.get("/", async (req, res) => {
+  try {
+    const carros = await knex("carros");
+    return res.json(carros);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensagem: "Erro do servidor" });
+  }
+});
 
 const port = process.env.PORT || 3000;
 
